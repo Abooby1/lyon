a78c7bb92af91eb978d00986d94d3f807399bf1d
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2024-01-07 17:49:08 +0000

    1.0 fixes

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 import axios from 'axios'
-import { ClientAuth } from './index.js'
+import { ClientAuth, Config } from './index.js'
 import FormData from 'form-data'
 
 export async function request(method, url, body, auth, contentType = "application/json", stringify = true, useJson = false) {
@@ -24,36 +24,38 @@ export async function request(method, url, body, auth, contentType = "applicatio
 		let response = await fetch('https://photop.exotek.co/' + url, data);
 		resolve([response.status, await response.text()])
 
-		if(url.startsWith('posts/new')) {
+		if(Config.LyonStats) {
+			if(url.startsWith('posts/new')) {
+				try{
+					axios({
+						method: 'POST',
+						url: 'https://lyonbackend.cyclic.app/statistics/add?type=posts',
+						headers: {
+							bottoken: ClientAuth
+						}
+					})
+				}catch{}
+			} else if(url.startsWith('chats/new')) {
+				try{
+					axios({
+						method: 'POST',
+						url: 'https://lyonbackend.cyclic.app/statistics/add?type=chats',
+						headers: {
+							bottoken: ClientAuth
+						}
+					})
+				}catch{}
+			}
+
 			try{
 				axios({
 					method: 'POST',
-					url: 'https://lyonbackend.cyclic.app/statistics/add?type=posts',
-					headers: {
-						bottoken: ClientAuth
-					}
-				})
-			}catch{}
-		} else if(url.startsWith('chats/new')) {
-			try{
-				axios({
-					method: 'POST',
-					url: 'https://lyonbackend.cyclic.app/statistics/add?type=chats',
+					url: 'https://lyonbackend.cyclic.app/statistics/add?type=fetches',
 					headers: {
 						bottoken: ClientAuth
 					}
 				})
 			}catch{}
 		}
-		
-		try{
-			axios({
-				method: 'POST',
-				url: 'https://lyonbackend.cyclic.app/statistics/add?type=fetches',
-				headers: {
-					bottoken: ClientAuth
-				}
-			})
-		}catch{}
 	})
 }
